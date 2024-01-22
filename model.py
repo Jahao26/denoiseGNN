ab427867832142c32fe2d39715d977c5398ef208
Jahz <62499863+Jahao26@users.noreply.github.com>
2024-01-22 22:24:25 +0800

    Update model.py

--- model.py
+++ model.py
@@ -26,25 +26,22 @@ class myModel(nn.Module):
         self.activation = nn.PReLU()
         self.gnn_layers = args.gnn_layers
 
-        gats = []  # 通过这个rgcn，得到注意力作为边的权重
+        gats = []
         for _ in range(args.gnn_layers):
             gats += [GAT(args.hidden_dim)]
         self.gather = nn.ModuleList(gats)
 
         self.fc1 = nn.Linear(args.emb_dim, args.hidden_dim)
 
-        self.graph_dim = args.hidden_dim * (args.gnn_layers + 1)  # + args.emb_dim
+        self.graph_dim = args.hidden_dim * (args.gnn_layers + 1)
         self.in_dim = args.hidden_dim * (args.gnn_layers + 1) + args.emb_dim
-        # layers = [nn.Linear(self.graph_dim, args.hidden_dim), nn.ReLU()]
 
-        # 添加的Syn-LSTM层
         self.enhance = nn.Linear(args.emb_dim, args.emb_dim, bias=True)
         self.graph_enhance = nn.Linear(self.graph_dim, self.graph_dim, bias=True)
-        self.lstm_f = MyLSTM(args.emb_dim, self.lstm_hidden_dim, self.graph_dim)  # 添加的Syn-LSTM层
-        # self.lstm_f = MyLSTM(self.graph_dim, self.lstm_hidden_dim, args.emb_dim)
+        self.lstm_f = MyLSTM(args.emb_dim, self.lstm_hidden_dim, self.graph_dim) 
 
         # output mlp layers
-        layers = [nn.Linear(self.lstm_hidden_dim, args.hidden_dim), nn.PReLU()]  # 添加的Syn-LSTM层
+        layers = [nn.Linear(self.lstm_hidden_dim, args.hidden_dim), nn.PReLU()]
         for _ in range(args.mlp_layers - 1):
             layers += [nn.Linear(args.hidden_dim, args.hidden_dim), nn.PReLU()]
         layers += [self.dropout]
@@ -74,9 +71,8 @@ class myModel(nn.Module):
                     H_layer = H_temp
                 else:
                     H_layer = torch.cat((H_layer, H_temp), dim=1)
-            H.append(H_layer)   # 上一层将会发生情感误判的节点信息将会继承到下一层的节点中，导致情感信息无法得到纠正（这一点可以试一试）
+            H.append(H_layer) 
         H = torch.cat(H, dim=2)
-        # H = self.activation(self.graph_enhance(H))
         features = self.activation(self.enhance(features))
         H = self.lstm_f(features, H)
 
@@ -88,7 +84,6 @@ class myModel(nn.Module):
 class ablationModel(nn.Module):
     def __init__(self, args, num_class):
         super().__init__()
-        # self.encoder = AutoModel.from_pretrained(args.bert_path)
         self.args = args
         self.filter = context_filter()
 
@@ -98,25 +93,22 @@ class ablationModel(nn.Module):
         self.activation = nn.PReLU()
         self.gnn_layers = args.gnn_layers
 
-        gats = []  # 通过这个rgcn，得到注意力作为边的权重
+        gats = [] 
         for _ in range(args.gnn_layers):
             gats += [GAT_dialoggcn_v1(args.hidden_dim)]
         self.gather = nn.ModuleList(gats)
 
         self.fc1 = nn.Linear(args.emb_dim, args.hidden_dim)
 
-        self.graph_dim = args.hidden_dim * (args.gnn_layers + 1)  # + args.emb_dim
+        self.graph_dim = args.hidden_dim * (args.gnn_layers + 1) 
         self.in_dim = args.hidden_dim * (args.gnn_layers + 1) + args.emb_dim
-        # layers = [nn.Linear(self.graph_dim, args.hidden_dim), nn.ReLU()]
 
-        # 添加的Syn-LSTM层
         self.enhance = nn.Linear(args.emb_dim, args.emb_dim, bias=True)
         self.graph_enhance = nn.Linear(self.graph_dim, self.graph_dim, bias=True)
-        self.lstm_f = MyLSTM(args.emb_dim, self.lstm_hidden_dim, self.graph_dim)  # 添加的Syn-LSTM层
-        # self.lstm_f = MyLSTM(self.graph_dim, self.lstm_hidden_dim, args.emb_dim)
+        self.lstm_f = MyLSTM(args.emb_dim, self.lstm_hidden_dim, self.graph_dim) 
 
         # output mlp layers
-        layers = [nn.Linear(args.emb_dim + self.graph_dim, args.hidden_dim), nn.PReLU()]  # 添加的Syn-LSTM层
+        layers = [nn.Linear(args.emb_dim + self.graph_dim, args.hidden_dim), nn.PReLU()]
         for _ in range(args.mlp_layers - 1):
             layers += [nn.Linear(args.hidden_dim, args.hidden_dim), nn.PReLU()]
         layers += [self.dropout]
@@ -133,13 +125,12 @@ class ablationModel(nn.Module):
         :return:
         '''
         num_utter = features.size()[1]
-        adj = self.filter(features, entro, alpha, threshold)  # 邻接矩阵mask
-        H0 = self.activation(self.fc1(features))  # -> (batch, seq_len, 300)
-        # H0 = self.dropout(H0)
+        adj = self.filter(features, entro, alpha, threshold) 
+        H0 = self.activation(self.fc1(features))  
         H = [H0]
-        for l in range(self.args.gnn_layers):  # H为每层节点张量堆叠
+        for l in range(self.args.gnn_layers): 
             for i in range(0, num_utter):
-                _, M = self.gather[l](H[l][:, i, :], H[l], H[l], adj[:, i, :], s_mask[:, i, :])  # M.size(64, 300)
+                _, M = self.gather[l](H[l][:, i, :], H[l], H[l], adj[:, i, :], s_mask[:, i, :]) 
                 H_temp = M.unsqueeze(1)
                 if i == 0:
                     H_layer = H_temp
@@ -162,8 +153,6 @@ class ft_model(nn.Module):
         self.designed = myModel(args, n_class)
         self.encoder.load_state_dict(bertcheckpoint['model_state_dict'])
         self.designed.load_state_dict(mymodelcheckpoint['model_state_dict'])
-        # for param in self.encoder.parameters():
-        #     param.requires_grad = False
 
     def forward(self, batch_tokens, s_mask, entro, alpha, threshold):
         '''
